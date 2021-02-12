4ff0368bbceb0ba919e24ce70a4790d5ee68a4ef
thenameismohiitt <mohitchandani50@yahoo.in>
2021-02-12 22:09:31 +0530

    Added Linkedin login and minor fixes

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import FacebookLogin from "react-facebook-login";
 import GoogleLogin from 'react-google-login';
+import {LinkedIn} from 'react-linkedin-login-oauth2';
+import linkedin from 'react-linkedin-login-oauth2/assets/linkedin.png'
 
 
 export default class SignUp extends Component {
@@ -10,6 +12,8 @@ export default class SignUp extends Component {
         name: "",
         email: "",
         picture: "",
+        code: '',
+        errorMessage: '',
     };
     componentClicked = () => console.log("You just logged in ");
 
@@ -21,7 +25,65 @@ export default class SignUp extends Component {
         console.log(response)
     }
 
+
+    handleSuccess = (data) => {
+        this.setState({
+            code: data.code,
+            errorMessage: '',
+        });
+    }
+
+    handleFailure = (error) => {
+        this.setState({
+            code: '',
+            errorMessage: error.errorMessage,
+        });
+    }
+
     render() {
+        let fbContent;
+        let googleContent;
+        let linkedinContent;
+
+        if (this.state.isLoggedIn) {
+            fbContent = null;
+        } else {
+            fbContent = (
+                <FacebookLogin
+                    appId="222693546169490"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    cssClass="btn btn-primary btn-block"
+                    onClick={this.componentClicked}
+                    callback={this.responseFacebook}
+                />
+            );
+
+            googleContent = (
+                <GoogleLogin
+                    clientId="643966313203-7s8kqjlohqkdqp8ge8cciafnoi2gppc6.apps.googleusercontent.com"
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick} className="btn btn-primary btn-block"
+                        >Google Login</button>
+                    )}
+                    onSuccess={this.responseGoogle}
+                    onFailure={this.responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                    buttonText="Login"
+                />
+            );
+
+            linkedinContent = (
+                <LinkedIn
+                    clientId="81lx5we2omq9xh"
+                    onFailure={this.handleFailure}
+                    onSuccess={this.handleSuccess}
+                    redirectUri="https://localhost:3000/linkedin"
+                >
+                    <img src={linkedin} alt="Log in with Linked In" style={{maxWidth: '180px'}}/>
+                </LinkedIn>
+            );
+        }
         return (
             <form>
                 <h3>Sign Up</h3>
@@ -57,29 +119,11 @@ export default class SignUp extends Component {
                     Sign Up
                 </button>
                 <br></br>
-                <div><FacebookLogin
-                    appId="222693546169490"
-                    autoLoad={true}
-                    fields="name,email,picture"
-                    cssClass="btn btn-primary btn-block"
-                    onClick={this.componentClicked}
-                    callback={this.responseFacebook}
-                />
-                </div>
+                <div>{fbContent}</div>
                 <br/>
-                <div>
-                    <GoogleLogin
-                        clientId="643966313203-7s8kqjlohqkdqp8ge8cciafnoi2gppc6.apps.googleusercontent.com"
-                        render={renderProps => (
-                            <button onClick={renderProps.onClick} className="btn btn-primary btn-block"
-                            >Google Login</button>
-                        )}
-                        onSuccess={this.responseGoogle}
-                        onFailure={this.responseGoogle}
-                        cookiePolicy={'single_host_origin'}
-                        buttonText="Login"
-                    />
-                </div>
+                <div>{googleContent}</div>
+                {/*<br/>*/}
+                {/*<div>{linkedinContent}</div>*/}
                 <p className="forgot-password text-right">
                     Already registered <a href="#">sign in?</a>
                 </p>
